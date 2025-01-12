36540806f553505413936372de534d5ef440ec9b
Nima4180 <146990855+Nima4180@users.noreply.github.com>
2025-01-12 01:13:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('language-switcher').addEventListener('change', (event) => {
+    const selectedLanguage = event.target.value;
+    alert(`Switching to ${selectedLanguage}`);
+    // Add functionality for changing website language here.
+});
